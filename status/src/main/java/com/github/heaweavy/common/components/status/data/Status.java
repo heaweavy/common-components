113e80bbad746a83e4edf1cf25d2da25590ed287
caimubin <511102536@qq.com>
2016-10-08 09:14:22 +0800

    status framework

---
+++ status/src/main/java/com/github/heaweavy/common/components/status/data/Status.java
@@ -0,0 +1,7 @@
+package com.github.heaweavy.common.components.status.data;
+
+/**
+ * Created by heaweavy on 10/6/2016.
+ */
+public class Status {
+}
---
+++ status/src/main/java/com/github/heaweavy/common/components/status/task/DataDestroyTask.java
@@ -0,0 +1,7 @@
+package com.github.heaweavy.common.components.status.task;
+
+/**
+ * Created by heaweavy on 10/6/2016.
+ */
+public class DataDestroyTask {
+}
